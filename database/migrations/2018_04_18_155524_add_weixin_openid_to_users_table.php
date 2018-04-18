e8518e864a927e25c00857a6f06132dff80bb521
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-04-18 15:58:56 +0800

    为 users 表增加两个字段，weixin_openid，weixin_unionid

---
+++ database/migrations/2018_04_18_155524_add_weixin_openid_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWeixinOpenidToUsersTable extends Migration
+{
+    /**
+     * 首先需要为 users 表增加两个字段，weixin_openid，weixin_unionid，
+     * 用来记录微信用户的唯一标识。修改 password 字段为 nullable，因为第三方登录不需要密码。
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('weixin_openid')->unique()->nullable()->after('password');
+            $table->string('weixin_unionid')->unique()->nullable()->after('weixin_openid');
+            $table->string('password')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('weixin_openid');
+            $table->dropColumn('weixin_unionid');
+            $table->string('password')->nullable(false)->change();
+        });
+    }
+}
